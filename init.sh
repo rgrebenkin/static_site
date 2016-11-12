04c39246875d48f37efd1384e290212dd6e5a4b6
rgrebenkin <rgrebenkin@mail.ru>
2016-11-12 21:05:19 +0300

    commit

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ rm -r ~/web
 mkdir -p ~/web/{etc,uploads,public/{img,js,css}}/
 cp nginx.conf ~/web/etc/nginx.conf
 sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
+sudo /etc/init.d/nginx start
